e8d85b3df410c52e5cc897528ab94014e953f8d7
LeonMack <59763958+LeonSRH@users.noreply.github.com>
2022-05-17 19:18:39 +0200

    Added prefabs + tool_test_scripts
    After they got removed for some reason

---
+++ Assets/Own_Assets/Tool.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tool_Template_NS;
+public class Tool : MonoBehaviour
+{
+    Tool_Selection tool_; 
+
+    void Start()
+    {
+        tool_.toolselection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
